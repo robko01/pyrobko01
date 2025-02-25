759fc9a48660d0a6e0cda2fd4f4ddd7e4a07d9be
Orlin Dimitrov <orlin369@gmail.com>
2025-02-25 18:33:41 +0200

    Fix gripper direction

--- robko01/tasks/task_ui_qt/gui.py
+++ robko01/tasks/task_ui_qt/gui.py
@@ -915,9 +915,9 @@ class GUI(QApplication):
         self.__window.btnRCCW.pressed.connect(self.__btnRCCW_pressed)
         self.__window.btnRCCW.released.connect(self.__diff_stop)
 
-        self.__window.btnGripperOpen.pressed.connect(self.__axis_controllers[5].set_cw)
+        self.__window.btnGripperOpen.pressed.connect(self.__axis_controllers[5].set_ccw)
         self.__window.btnGripperOpen.released.connect(self.__axis_controllers[5].stop)
-        self.__window.btnGripperClose.pressed.connect(self.__axis_controllers[5].set_ccw)
+        self.__window.btnGripperClose.pressed.connect(self.__axis_controllers[5].set_cw)
         self.__window.btnGripperClose.released.connect(self.__axis_controllers[5].stop)
 
         # Manual Positioning Control
