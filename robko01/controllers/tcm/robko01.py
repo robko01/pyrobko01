46a2a182afc16fcb0a49923d388085f8d7a58f83
Orlin Dimitrov <orlin369@gmail.com>
2025-02-26 21:34:58 +0200

    Cleanup code

--- robko01/controllers/tcm/robko01.py
+++ robko01/controllers/tcm/robko01.py
@@ -79,6 +79,7 @@ class Robko01(BaseRobko01):
         self.__is_moving_cb = None
         """Is moving callback.
         """
+
 #endregion
 
 #region Public Methods
@@ -92,45 +93,6 @@ class Robko01(BaseRobko01):
         """Disconnect from robot controller.
         """
         self.__communicator.disconnect()
-
-    def ping(self, payload):
-        """Ping the robot controller.
-
-        Args:
-            payload (bytes): Ping payload.
-
-        Raises:
-            ValueError: Payload can not be None.
-            InvalidOperationCode: Invalid operation code.
-            InvalidStatusCode: Invalid status code.
-            InvalidPackage: Invalid package code.
-
-        Returns:
-            bytes: Answer payload.
-        """
-        if payload is None:
-            raise ValueError("Payload can not be None")
-
-        response = None
-
-        while True:
-            response = self.__pm.request(OpCode.Ping.value, payload)
-            if response.is_valid():
-                if response.status == StatusCode.Ok.value:
-                    if response.opcode == OpCode.Ping.value:
-                        break
-                    else:
-                        raise InvalidOperationCode("Operation code: {}".format(response.opcode))
-                else:
-                    raise InvalidStatusCode("Status: {}; OpCode: {}".format(\
-                        StatusCode.to_text(response.status),\
-                        OpCode.to_text(response.opcode)))
-            else:
-                raise InvalidPackage("Invalid package.")
-
-            time.sleep(self._sync_interval)
-
-        return response
 
     def stop(self):
         """Stop robot motion execution.
@@ -257,53 +219,6 @@ class Robko01(BaseRobko01):
                 raise InvalidPackage("Invalid package.")
 
             time.sleep(self._sync_interval)
-
-        return response
-
-    def move_relative(self, current_point):
-        """Move relative to next robot position.
-
-        Args:
-            current_point (list): New robot position.
-
-        Raises:
-            InvalidOperationCode: Invalid operation code.
-            InvalidStatusCode: Invalid status code.
-            InvalidPackage: Invalid package code.
-
-        Returns:
-            any: Communicator response.
-        """
-        response = None
-
-        while True:
-
-            payload = pack("<hhhhhhhhhhhh",\
-                int(current_point[0]), int(current_point[1]),\
-                int(current_point[2]), int(current_point[3]),\
-                int(current_point[4]), int(current_point[5]),\
-                int(current_point[6]), int(current_point[7]),\
-                int(current_point[8]), int(current_point[9]),\
-                int(current_point[10]), int(current_point[11]))
-
-            response = self.__pm.request(OpCode.MoveRelative.value, payload)
-            if response.is_valid():
-
-                if response.status == StatusCode.Ok.value:
-                    if response.opcode == OpCode.MoveRelative.value:
-                        break
-                    else:
-                        raise InvalidOperationCode("Operation code: {}".format(response.opcode))
-
-                elif response.status == StatusCode.Busy.value:
-                    break
-
-                else:
-                    raise InvalidStatusCode("Status: {}; OpCode: {}".format(\
-                        StatusCode.to_text(response.status),\
-                        OpCode.to_text(response.opcode)))
-            else:
-                raise InvalidPackage("Invalid package.")
 
         return response
 
