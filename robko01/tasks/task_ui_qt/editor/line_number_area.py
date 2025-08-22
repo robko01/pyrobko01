65a7ce170d65816658a35ac9a23e594251618225
Orlin Dimitrov <orlin369@gmail.com>
2025-08-22 18:43:19 +0300

    Add line numbering classes

---
+++ robko01/tasks/task_ui_qt/editor/line_number_area.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+
+Robko 01 - Python Control Software
+
+Copyright (C) [2020] [Orlin Dimitrov]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, Orlin Dimitrov"
+"""Copyright holder"""
+
+__credits__ = []
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "robko01@8bitclub.com"
+"""E-mail of the author."""
+
+__status__ = "Debug"
+"""File status."""
+
+#endregion
+
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QSize
+
+class LineNumberArea(QWidget):
+    def __init__(self, editor):
+        super().__init__(editor)
+        self.code_editor = editor
+
+    def sizeHint(self):
+        return QSize(self.code_editor.line_number_area_width(), 0)
+
+    def paintEvent(self, event):
+        self.code_editor.line_number_area_paint_event(event)
